62f5ed7e78ae3c7020b48184ec13115d7c53c2d2
Ruvik Perera <ruvikp@trafforge.com>
2025-12-17 16:54:22 +0530

    added the backend health check for th GCP load balancer

--- server.js
+++ server.js
@@ -29,9 +29,14 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// Health check route for GCP Load Balancer
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 // Start Server
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
